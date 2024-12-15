ad8eff7a6e11cb0b41841ae40d536b810f38af94
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 14:08:54 +0100

    @RequestBody

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/validation/ValidationController.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/validation/ValidationController.java
@@ -1,8 +1,16 @@
 package com.javaTirane42.sda_spring_frmework_java_tr_42.validation;
 
 
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class ValidationController {
+
+    @PostMapping("person")
+    public Person addPerson (@Valid @RequestBody  Person person){
+        return person;
+    }
 }
