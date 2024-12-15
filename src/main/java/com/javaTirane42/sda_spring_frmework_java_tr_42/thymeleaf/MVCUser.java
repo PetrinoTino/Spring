d6fea54be8a3e9fc471d9d0a4861ed48e60fa6c5
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 15:08:36 +0100

    mvc user

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/thymeleaf/MVCUser.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/thymeleaf/MVCUser.java
@@ -1,4 +1,12 @@
 package com.javaTirane42.sda_spring_frmework_java_tr_42.thymeleaf;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "mvc_users")
 public class MVCUser {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 }
