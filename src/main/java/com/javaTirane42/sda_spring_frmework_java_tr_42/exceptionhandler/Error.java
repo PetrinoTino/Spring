ff04d44708c840f6bb562fca38c971066a879d6b
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 13:18:31 +0100

    get/set

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/exceptionhandler/Error.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/exceptionhandler/Error.java
@@ -1,4 +1,18 @@
 package com.javaTirane42.sda_spring_frmework_java_tr_42.exceptionhandler;
 
 public class Error {
+
+    private String message;
+
+    public Error(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
