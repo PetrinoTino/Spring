dbae366cf89325542b7f79b4fa897ce96718fe3b
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 13:27:20 +0100

    directory

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/exceptionhandler/LocalExceptionController.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/exceptionhandler/LocalExceptionController.java
@@ -1,12 +1,20 @@
 package com.javaTirane42.sda_spring_frmework_java_tr_42.exceptionhandler;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LocalExceptionController {
     @GetMapping("resources")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void showResources(){
         throw  new SdaException("Resource Exception");
+    }
+    @GetMapping("resources/directory");
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public  void showDirectory(){
+        throw new SpecificSdaException("Directory Exception");
     }
 }
