b555c3a2b99f6071ae90fbb678c956934a6f9152
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 13:35:55 +0100

    restcontrolleradvice

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/exceptionhandler/GlobalExceptionsHandler.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/exceptionhandler/GlobalExceptionsHandler.java
@@ -1,4 +1,11 @@
 package com.javaTirane42.sda_spring_frmework_java_tr_42.exceptionhandler;
 
+
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
 public class GlobalExceptionsHandler {
+
+
 }
