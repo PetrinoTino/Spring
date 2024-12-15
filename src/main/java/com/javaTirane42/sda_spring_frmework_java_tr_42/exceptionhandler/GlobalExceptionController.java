f685aba620c6de6acead27bc56af5b3ca40cd208
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 13:40:27 +0100

    Global Exception

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/exceptionhandler/GlobalExceptionController.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/exceptionhandler/GlobalExceptionController.java
@@ -1,7 +1,12 @@
 package com.javaTirane42.sda_spring_frmework_java_tr_42.exceptionhandler;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GlobalExceptionController {
+    @GetMapping("global-sda")
+    public void runGlobalSda(){
+        throw new SdaException("Global SDA Exception");
+    }
 }
