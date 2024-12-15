b30da7b8f7ac035ed60ebf3e3d442474097196eb
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 15:17:09 +0100

    constructor

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/thymeleaf/MVCUserController.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/thymeleaf/MVCUserController.java
@@ -1,4 +1,12 @@
 package com.javaTirane42.sda_spring_frmework_java_tr_42.thymeleaf;
 
+import org.springframework.stereotype.Controller;
+
+@Controller
 public class MVCUserController {
+    private MVCUserController mvcUserController;
+
+    public MVCUserController(MVCUserController mvcUserController) {
+        this.mvcUserController = mvcUserController;
+    }
 }
