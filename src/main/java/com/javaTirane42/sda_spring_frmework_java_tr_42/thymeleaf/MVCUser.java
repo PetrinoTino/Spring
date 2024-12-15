e5c15352615c52ff6debb7b34af0463e5ea697e4
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 15:09:41 +0100

    mvc users

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/thymeleaf/MVCUser.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/thymeleaf/MVCUser.java
@@ -12,4 +12,32 @@ public class MVCUser {
     private Long id;
 
     @NotBlank(message = "Name is mandatory")
+    private String name;
+
+    @NotBlank(message = "Email is mandatory")
+    private String email;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
