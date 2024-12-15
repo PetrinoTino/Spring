42c43118990084b1feb23877673f65636a24ae5f
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 15:20:05 +0100

    model spring

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/thymeleaf/MVCUserController.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/thymeleaf/MVCUserController.java
@@ -1,12 +1,64 @@
 package com.javaTirane42.sda_spring_frmework_java_tr_42.thymeleaf;
 
+
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class MVCUserController {
-    private MVCUserController mvcUserController;
 
-    public MVCUserController(MVCUserController mvcUserController) {
-        this.mvcUserController = mvcUserController;
+    private MVCUserRepository mvcUserRepository;
+
+    public MVCUserController(MVCUserRepository mvcUserRepository) {
+        this.mvcUserRepository = mvcUserRepository;
+    }
+
+    @GetMapping("/signup")
+    public String showSignUpForm(MVCUser user) {
+        return "add-user";
+    }
+
+    @PostMapping("/adduser")
+    public String addUser(@Valid MVCUser user, BindingResult bindingResult, Model model) {
+        if (bindingResult.hasErrors()) {
+            return "add-user";
+        }
+
+        mvcUserRepository.save(user);
+        return "redirect:/index";
+    }
+
+    @GetMapping("/index")
+    public String showUserList(Model model) {
+        model.addAttribute("users", mvcUserRepository.findAll());
+        return "index";
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
