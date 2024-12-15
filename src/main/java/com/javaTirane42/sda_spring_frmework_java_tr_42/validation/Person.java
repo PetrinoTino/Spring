7636ab548b969c5e83cac97326a76281fa344101
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 13:59:46 +0100

    person class constructor

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/validation/Person.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/validation/Person.java
@@ -1,4 +1,37 @@
 package com.javaTirane42.sda_spring_frmework_java_tr_42.validation;
 
 public class Person {
+    private String firstname;
+    private String email;
+    private  String address;
+
+    public Person(String firstname, String email, String address) {
+        this.firstname = firstname;
+        this.email = email;
+        this.address = address;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 }
