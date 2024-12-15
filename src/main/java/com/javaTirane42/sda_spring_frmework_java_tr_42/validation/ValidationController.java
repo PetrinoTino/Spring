e1c654b817d7b8ce4ef0e0ba0350ad3635b98755
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 14:07:11 +0100

    validationcontroller

---
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/validation/ValidationController.java
@@ -0,0 +1,8 @@
+package com.javaTirane42.sda_spring_frmework_java_tr_42.validation;
+
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ValidationController {
+}
