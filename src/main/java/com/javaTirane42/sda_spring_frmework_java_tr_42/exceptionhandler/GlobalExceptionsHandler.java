b03ddf50ddc1fb81f63372c7681226a541407164
Petrino Qasolli <petrinotino@gmail.com>
2024-12-15 13:37:03 +0100

    Error

--- src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/exceptionhandler/GlobalExceptionsHandler.java
+++ src/main/java/com/javaTirane42/sda_spring_frmework_java_tr_42/exceptionhandler/GlobalExceptionsHandler.java
@@ -1,11 +1,15 @@
 package com.javaTirane42.sda_spring_frmework_java_tr_42.exceptionhandler;
 
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestControllerAdvice
 public class GlobalExceptionsHandler {
-
+@ExceptionHandler(SdaException.class)
+    public Error handleSdaException(SdaException e){
+    return new Error(e.getMessage());
+}
 
 }
